0013a5bc8700337a37a5cae0d04ff500b523cf04
James Austin <james.austin@trafford.gov.uk>
2020-09-10 12:03:32 +0100

    Accessibility improvements: Added landmark tags, sorted the header order, added alt text to the dynamic plot, improved the colour contrast issues and fixed label issues with form elements.

--- app.R
+++ app.R
@@ -14,42 +14,113 @@ lookup <- fromJSON(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/
   select(lad_code = attributes.LAD18CD, lad_name = attributes.LAD18NM,
          ward_code = attributes.WD18CD, ward_name = attributes.WD18NM)
 
-ui <- fluidPage(tags$head(includeCSS("styles.css")),
-   titlePanel(title = NULL, windowTitle = "Postcode loader"),
-   sidebarLayout(
-      sidebarPanel(
-        tags$a(tags$img(src = "trafforddatalab_logo.png", width = "25%"), href = "https://www.trafforddatalab.io/", target="_blank"),
-        tags$h3("Postcode loader"),
-        tags$p("This application allows you to download the ",
-               tags$a("latest postcode centroids", href = "https://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-latest-centroids", target="_blank"),
-               "from the ", tags$a("ONS", href = "https://www.ons.gov.uk/", target="_blank"), "' ", 
-               tags$a("Open Geography Portal", href = "https://geoportal.statistics.gov.uk/", target="_blank"), 
-               " for your chosen ward in Greater Manchester."),
-        tags$br(),
-        selectizeInput(
-          inputId = "filter_lad",
-          label = "Choose a local authority",
-          choices = sort(unique(lookup$lad_name))),
-        uiOutput("filter_ward"),
-        tags$br()),
-        mainPanel(uiOutput("text"),
-                plotOutput("map"),
-                DT::dataTableOutput("table"),
-                br(),
-                textOutput("attribution"),
-                uiOutput("code")
+ui <- fluidPage(title = "Postcode loader",
+   # Set the language of the page - important for accessibility
+   tags$html(lang = "en-GB"),
+   tags$head(includeCSS("styles.css")),
+   br(),
+     sidebarLayout(
+       tags$header(
+        sidebarPanel(
+            tags$a(tags$img(src = "trafforddatalab_logo.png", width = "25%", alt = "Trafford Data Lab"), href = "https://www.trafforddatalab.io/", target="_blank"),
+            tags$h1("Postcode loader"),
+            tags$p("This application allows you to download the ",
+                    tags$a("latest postcode centroids", href = "https://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-latest-centroids", target="_blank"),
+                    "from the ", tags$a("ONS", href = "https://www.ons.gov.uk/", target="_blank"), "' ", 
+                    tags$a("Open Geography Portal", href = "https://geoportal.statistics.gov.uk/", target="_blank"), 
+                    " for your chosen ward in Greater Manchester."),
+            tags$br(),
+            selectizeInput(
+              inputId = "filter_lad",
+              label = "Choose a local authority",
+              choices = sort(unique(lookup$lad_name))),
+            uiOutput("filter_ward_container"),
+            tags$br()
+          )
+        ),
+        tags$main(
+          mainPanel(uiOutput("text"),
+                  plotOutput("map"),
+                  DT::dataTableOutput("table"),
+                  br(),
+                  textOutput("attribution"),
+                  uiOutput("code")
+        )
       )
-   )
+   ),
+   HTML("
+        <script>
+          // Receive call from Shiny server with the alt text for the dynamic plot img.
+          Shiny.addCustomMessageHandler('altTextHandler', function(altText) {
+              // Setup a call to the update function every 100 milliseconds in case the plot has not been created yet
+              var altTextCallback = setInterval(function() {
+                  // The plot img element does not have an id itself so we can only identify it from the parent node
+                  try {
+                      var plotContainer = document.getElementById('map');
+                      plotContainer.firstChild.setAttribute('alt', altText);
+                      clearInterval(altTextCallback); // Cancel the callback as we have updated the alt text
+                  }
+                  catch(e) {
+                      // An error occurred, likely the img tag hasn't been created/replaced yet. Function will run again in 100 milliseconds
+                  }
+              }, 100);
+          });
+          
+          // Add label to the hidden select element for the LA choice
+          var cb_selectLabelLa = setInterval(function() {
+              try {
+                  // create a new label tag to hold the hidden select tag
+                  var label = document.createElement('label');
+                  label.setAttribute('style', 'display: none;');
+                  
+                  // get the references to the hidden select tag and its parent
+                  var selection = document.getElementById('filter_lad');
+                  var parent = selection.parentNode;
+                  
+                  // add the select to the label and then the label to the parent div
+                  label.appendChild(selection);
+                  parent.appendChild(label);
+                  
+                  clearInterval(cb_selectLabelLa); // cancel further calls to this fn
+              }
+              catch(e) {
+                  // do nothing, wait until function is called again next interval
+              }
+          }, 500);
+          
+          // Add label to the hidden select element for the ward choice
+          var cb_selectLabelWard = setInterval(function() {
+              try {
+                  // create a new label tag to hold the hidden select tag
+                  var label = document.createElement('label');
+                  label.setAttribute('style', 'display: none;');
+                  
+                  // get the references to the hidden select tag and its parent
+                  var selection = document.getElementById('filter_ward');
+                  var parent = selection.parentNode;
+                  
+                  // add the select to the label and then the label to the parent div
+                  label.appendChild(selection);
+                  parent.appendChild(label);
+                  
+                  clearInterval(cb_selectLabelWard); // cancel further calls to this fn
+              }
+              catch(e) {
+                  // do nothing, wait until function is called again next interval
+              }
+          }, 500);
+        </script>
+    ")
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   filtered_data <- reactive({
     filter(lookup, lad_name == input$filter_lad)
   })
   
   
-  output$filter_ward <- renderUI({
+  output$filter_ward_container <- renderUI({
     selectizeInput("filter_ward", "and a ward", sort(unique(filtered_data()$ward_name)))
   })
   
@@ -99,7 +170,7 @@ server <- function(input, output) {
   output$map = renderPlot({
     req(ward_code())
     plot(st_geometry(lad_layer()), col = "#DDDDCC", border = "#212121")
-    plot(st_geometry(ward_layer()), add = T, col = "#fc6721", border = "#212121")
+    plot(st_geometry(ward_layer()), add = T, col = "#046dc3", border = "#212121")
 })
   
   output$table <- DT::renderDataTable(server=FALSE,{
@@ -126,7 +197,7 @@ server <- function(input, output) {
     req(ward_code())
     text <- paste0(pull(select(filter(filtered_data(), ward_name %in% input$filter_ward), ward_name)),
            " ward in ", input$filter_lad) 
-    tags$h4(text)
+    tags$h2(text)
   })
   
   output$attribution <- renderText({
@@ -136,7 +207,14 @@ server <- function(input, output) {
   
   output$code <- renderUI({
     req(ward_code())
-    tags$p("View code", a(icon("github"), href = "https://github.com/trafforddatalab/postcode_loader", target = "_blank"))
+    tags$p(a("View code ", icon("github"), href = "https://github.com/trafforddatalab/postcode_loader", target = "_blank"))
+  })
+  
+  observe({
+    # construct alt text string for the map plot and send to Javascript handler to add
+    altText <- paste0("Map showing location of ", pull(select(filter(filtered_data(), ward_name %in% input$filter_ward), ward_name)),
+                      " ward in ", input$filter_lad) 
+    session$sendCustomMessage("altTextHandler", altText)
   })
   
 }
